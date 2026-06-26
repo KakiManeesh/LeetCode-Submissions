7cc4a3442a2d5540c5b49d10ea44734247144213
Kaki Maneesh <maneeshkaki@gmail.com>
2026-06-26 16:38:26 +0530

    Time: 472 ms (39.70%) | Memory: 31.4 MB (74.07%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
@@ -1,15 +1,20 @@
-from collections import Counter
-
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
-        freq = Counter()
+        nums.sort()
+
+        i, j = 0, len(nums) - 1
         ans = 0
 
-        for x in nums:
-            if freq[k - x] > 0:
+        while i < j:
+            s = nums[i] + nums[j]
+
+            if s == k:
                 ans += 1
-                freq[k - x] -= 1
+                i += 1
+                j -= 1
+            elif s < k:
+                i += 1
             else:
-                freq[x] += 1
+                j -= 1
 
         return ans
