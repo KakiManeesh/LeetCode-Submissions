b5dfac21621cf6ab59b1ce01874f161236258347
Kaki Maneesh <maneeshkaki@gmail.com>
2026-07-09 23:50:05 +0530

    Time: 5 ms (84.97%) | Memory: 19.6 MB (92.01%) - LeetSync

--- 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.py
+++ 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.py
@@ -1,14 +1,15 @@
-import sys
-sys.set_int_max_str_digits(10**4)
 class Solution:
     def addToArrayForm(self, num: List[int], k: int) -> List[int]:
-        sum_ = 0
-        for i in num :
-            sum_ = sum_*10 + i
-        num = sum_
-        num = num + k
-        num = str(num)
-        num = list(num)
-        for i in range(len(num)) :
-            num[i] = int(num[i])
+        for i in range(len(num) - 1, -1, -1):
+            total = num[i] + k
+            num[i] = total % 10  
+            k = total // 10      
+            
+            if not k:
+                break
+        
+        while k:
+            num.insert(0, k % 10)
+            k //= 10
+            
         return num
