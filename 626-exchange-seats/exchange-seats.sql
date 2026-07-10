aab1e249459c7fb22ca1b73fef0ed006a8965e21
Kaki Maneesh <maneeshkaki@gmail.com>
2026-07-10 23:58:18 +0530

    Time: 382 ms (30.90%) | Memory: 0B (100.00%) - LeetSync

---
+++ 626-exchange-seats/exchange-seats.sql
@@ -0,0 +1,9 @@
+select 
+    case
+        when id % 2 = 0 then id - 1
+        when id = (select count(*) from Seat ) then id 
+        else id +1
+    end as id ,
+    student
+from Seat 
+order by id
