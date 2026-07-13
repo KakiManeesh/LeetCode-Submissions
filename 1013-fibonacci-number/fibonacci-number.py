4271a1a62367593001fc1eabd03636130c5e80dc
Kaki Maneesh <maneeshkaki@gmail.com>
2026-07-13 14:17:12 +0530

    Time: 51 ms (48.25%) | Memory: 19.2 MB (56.92%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -0,0 +1,9 @@
+from functools import lru_cache
+class Solution:
+    def fib(self, n: int) -> int:
+        @lru_cache
+        def helper (n) :
+            if n == 0 or n ==1 :
+                return n
+            return helper(n-1) + helper(n-2)
+        return helper(n)
