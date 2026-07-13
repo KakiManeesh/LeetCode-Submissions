9e05d3005b1d678c31457e74e8286c4c987deeb7
Kaki Maneesh <maneeshkaki@gmail.com>
2026-07-13 17:57:23 +0530

    Time: 267 ms (20.89%) | Memory: 21.8 MB (36.56%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.py
@@ -0,0 +1,32 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        n = len(grid)
+        m = len(grid[0])
+        def helper(i,j):
+            
+            if i<0 or j<0 or i>=n or j >= m :
+                return 0
+            if grid[i][j] == '0' :
+                return 0
+            if grid[i][j] == '#' :
+                return 0
+
+            grid[i][j] = '#'
+
+            return (
+                1 +
+                helper(i+1,j)+
+                helper(i-1,j)+
+                helper(i,j+1)+
+                helper(i,j-1)
+            ) 
+
+
+        count = 0
+        for i in range(n):
+            for j in range(m):
+                if helper(i,j) >= 1 :
+                    count += 1
+        for  i in grid:
+            print(i)
+        return count
