bf10da9cc2ee3a8ab4d15230f4f352f84c6b5f1d
Kaki Maneesh <maneeshkaki@gmail.com>
2026-07-18 21:56:49 +0530

    Time: 4 ms (16.96%) | Memory: 19.8 MB (77.69%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.py
+++ 801-is-graph-bipartite/is-graph-bipartite.py
@@ -1,23 +1,23 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
         
-        color = [0]*len(graph)
+        color = [0]*len(graph)       
 
-
-        def dfs(node,c):
-            color[node] = c
-            
-            for neighbor in graph[node]:
-                if color[neighbor ] == c :
-                    return False
-                    
-                if color[neighbor ] == 0 :
-                    if not dfs(neighbor , -c ) :
-                        return False
-            return True
-
-        for i in range(len(graph)):
+        for i in range(len(color)):
             if color[i] == 0 :
-                if not dfs(i,1):
-                    return False
-        return True
+                stack = [i]
+                color[i] = 1
+                while stack :
+                    n = len(stack)
+
+                    for ____ in range(n):
+                        node = stack.pop(0)
+
+                        for j in graph[node]:
+                            if color[j] == color[node]:
+                                return False
+                            
+                            if color[j] == 0 :
+                                color[j] = -color[node]
+                                stack.append(j)
+        return True 
