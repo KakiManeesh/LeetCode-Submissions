0387a9564c045532585d5f50ebaca0f9adce7bb6
Kaki Maneesh <maneeshkaki@gmail.com>
2026-07-28 11:16:06 +0530

    Time: 313 ms (39.17%) | Memory: 20.7 MB (79.72%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.py
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.py
@@ -5,9 +5,23 @@ class Solution:
             middle = s[n//2]
         else:
             middle= ''
+        '''
         first = s[:n//2]
         first = "".join(sorted(first))
         
         ans = first + middle + first[::-1]
 
         return ans
+        '''
+
+        hash = {}
+
+        for i in s :
+            hash[i] = hash.get(i,0) + 1
+
+        first = ""
+
+        for i in range(97,97+26) :
+            if chr(i) in hash :
+                first += chr(i) * (hash[chr(i)]//2) 
+        return first + middle + first[::-1]
