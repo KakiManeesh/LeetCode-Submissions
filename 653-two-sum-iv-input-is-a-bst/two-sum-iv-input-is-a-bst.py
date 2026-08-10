81cc84f80d0a62cca3212ddf0992e92aec920e4e
Kaki Maneesh <maneeshkaki@gmail.com>
2026-08-10 22:27:55 +0530

    Time: 7 ms (42.81%) | Memory: 22 MB (66.95%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.py
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.py
@@ -6,16 +6,52 @@
 #         self.right = right
 class Solution:
     def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
-        visited = set()
+        
+        stack1 = []
+        stack2 = []
+        def push_left(curr,stack1):
+            while curr :
+                stack1.append(curr)
+                curr =  curr.left
+        push_left(root,stack1)
 
-        def solve(node):
-            if not node :
+        def push_right(curr,stack2):            
+            while curr :
+                stack2.append(curr)
+                curr =  curr.right
+        push_right(root,stack2)
+
+        def next(stack):
+            last = stack.pop()
+            push_left(last.right,stack)
+            return last
+        
+        def prev(stack):
+            last = stack.pop()
+            push_right( last.left , stack )
+            return last
+        
+        def peek_left():
+            return stack1[-1]
+        
+        def peek_right():
+            return stack2[-1]
+
+        
+        while stack1 and stack2 :
+            first_node = peek_left()
+            last_node = peek_right()
+
+            if first_node == last_node :
                 return False
-            
-            if k-node.val in visited :
-                return True
-            
-            visited.add(node.val)
-            return solve(node.left) or solve(node.right)
 
-        return solve(root)
+            first = first_node.val
+            last =  last_node.val
+
+            if (first + last) == k :
+                return True
+            elif first+last > k :
+                prev(stack2)
+            else:
+                next(stack1)
+        return False
